5296c76485347e9aa8df42fe1fea7411bed74721
adekunleomooba <adekunleforreal@gmail.com>
2022-12-08 04:21:51 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,20 +3,17 @@
 
 
 /**
- *
- * main - Entry point
- *
+*
+* main - Entry point
+*
 
 
- * Return: Always 0 (Success)
- */
+* Return: Always 0 (Success)
+*/
 
- int main(void)
-
+int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	return (0);
 
 }

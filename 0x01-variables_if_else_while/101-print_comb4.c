4a8aee23db4a93a44839a831938d0ef8a0e28baf
adekunleomooba <adekunleforreal@gmail.com>
2022-12-09 11:35:31 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,24 +15,24 @@ int main(void)
 	{
 		for (tens = '0'; tens <= '9'; tens++)
 		{
-			for (ones = '0'; ones <= '9'; ones++)
-			{
-				if (!((ones == tens) || (tens == hundreds) || 
-							(tens > ones) || (hundreds > tens))) /*eliminates repitition*/
+				for (ones = '0'; ones <= '9'; ones++)
 				{
-					putchar(hundreds);
-					putchar(tens);
-					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' && 
-								tens == '8')) /*adds commas and spaces*/
+          			        if (!((ones == tens) || (tens == hundreds) ||												                                                        (tens > ones) || (hundreds > tens))) /*eliminates repitition*/
 					{
-						putchar(',');
-						putchar(' ');
+						putchar(hundreds);
+						putchar(tens);
+						putchar(ones);
+						if (!(ones == '9' && hundreds == '7' && 
+									tens == '8')) /*adds commas and spaces*/
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 				}
-			}
 		}
 	}
 	putchar('\n');
 	return (0);
+
 }		

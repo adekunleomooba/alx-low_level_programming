2d557ab9949fd4cdc3367d7bf29cf17d073a70ad
adekunleomooba <adekunleforreal@gmail.com>
2022-12-14 04:12:43 -0800

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,18 +3,18 @@
  * main - print first fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0.
-*/
+ */
 
 int main(void)
 {
 	int count;
 	unsigned long n1 = 0, n2 = 1, sum;
-	
+
 	for (count = 0; count < 50; count++)
 	{
 		sum = n1 + n2;
 		printf("%lu", sum);
-	
+
 		n1 = n2;
 		n2 = sum;
 
@@ -23,6 +23,5 @@ int main(void)
 		else
 			printf(", ");
 	}
-
 	return (0);
 }

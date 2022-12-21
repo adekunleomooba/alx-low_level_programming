a07a7a8c3f5de31a491f5b59071a531f6c276a04
adekunleomooba <adekunleforreal@gmail.com>
2022-12-21 09:46:30 -0800

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,27 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * rot13 - encoder rot13
- * @s: pointer to string params
- *
- * Return: *s
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
  */
 
 char *rot13(char *s)
 {
-int i;
-int j;
-char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-for (i = 0; s[i] != '\0'; i++)
+int count = 0, i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+while (*(s + count) != '\0')
 {
-for (j = 0; j < 52; j++)
+for (i = 0; i < 52; i++)
 {
-if (s[i] == data1[j])
+if (*(s + count) == alphabet[i])
 {
-s[i] = datarot[j];
+*(s + count) = rot13[i];
 break;
 }
+}
+count++;
 }
 return (s);
 }

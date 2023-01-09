9bdc7febada5639582fcedd426c8be7eeaf5b015
adekunleomooba <adekunleforreal@gmail.com>
2023-01-09 04:52:26 -0800

    3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,14 @@
 #include "main.h"
-/**
- * _puts - print a string to stdout
- * @str: pointer parameter
- */
-
 void _puts(char *str)
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-
-	_putchar('\n');
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
